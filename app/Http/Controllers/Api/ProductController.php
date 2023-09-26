0c2933e7a41b55b1dbe20c419c37f94a3eae6202
Ahmed ali <58213692+Ibnelaiq@users.noreply.github.com>
2023-09-26 15:09:42 +0500

    Fresh Laravel

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return $this->sendResponse(ProductResource::collection($products), 'Products retrieved successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $product = Product::create($input);
+
+        return $this->sendResponse(new ProductResource($product), 'Product created successfully.');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+// use Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+     public function register(Request $request)
+     {
+         $validator = Validator::make($request->all(), [
+             'name' => 'required',
+             'email' => 'required|email',
+             'password' => 'required',
+            //  'c_password' => 'required|same:password',
+         ]);
+
+         if($validator->fails()){
+             return $this->sendError('Validation Error.', $validator->errors());
+         }
+
+         $input = $request->all();
+         $input['password'] = bcrypt($input['password']);
+         $user = User::create($input);
+         $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+         $success['name'] =  $user->name;
+
+         return $this->sendResponse($success, 'User register successfully.');
+     }
+
+     public function loginUser(Request $request): Response
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return Response(['message' => $validator->errors()],401);
+        }
+
+        if(Auth::attempt($request->all())){
+
+            $user = Auth::user();
+
+            $success =  $user->createToken('MyApp')->plainTextToken;
+
+            return Response(['token' => $success],200);
+        }
+
+        return Response(['message' => 'email or password wrong'],401);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function userDetails(): Response
+    {
+        if (Auth::check()) {
+
+            $user = Auth::user();
+
+            return Response(['data' => $user],200);
+        }
+
+        return Response(['data' => 'Unauthorized'],401);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function logout(): Response
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return Response(['data' => 'User Logout successfully.'],200);
+    }
+}
